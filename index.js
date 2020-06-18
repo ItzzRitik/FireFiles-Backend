3878601e683b262b636a53aba4037a30115e92ee
Ritik Srivastava <ritik.space@gmail.com>
2020-06-19 03:13:41 +0530

    update dependencies

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require('express'),
-    app = express(),
-    http = require('http'),
-    server = http.createServer(app),
-    bodyparser = require('body-parser'),
-    mongoose = require('mongoose'),
-    ip = require('ip'),
+	app = express(),
+	http = require('http'),
+	server = http.createServer(app),
+	bodyparser = require('body-parser'),
+	mongoose = require('mongoose'),
+	ip = require('ip'),
 	chalk = require('chalk'),
 
 	mongoUtils = require('./tools/mongoUtils'),
-    logger = require('./tools/logger'),
+	logger = require('./tools/logger'),
 	crypto = require('./tools/crypto');
 	
 require('dotenv').config();
 const env = process.env;
 
+app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
+app.use('/lib', express.static('node_modules'));
+app.use(bodyparser.json({ limit: '50mb' }));
+app.use(bodyparser.urlencoded({ limit: '50mb', extended: true, parameterLimit:50000 }));
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
+app.get('/', function(req, res) {
+	res.render('index');
+});
 
 server.listen(env.PORT || 8080, function() {
 	logger.clear();
