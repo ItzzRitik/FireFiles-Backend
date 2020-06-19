36bc93a98134501d2571cd8d4766726417953806
Ritik Srivastava <ritik.space@gmail.com>
2020-06-20 00:19:28 +0530

    schema update

--- models/user.js
+++ models/user.js
@@ -4,57 +4,39 @@ const mongoose = require('mongoose'),
 	validator = require('validator');
 
 let UserSchema = new mongoose.Schema({
-	name: String,
-	email: {
-		type: String,
-		trim: true,
-		lowercase: true,
-		unique: true,
-		index: { unique: true },
-		required: 'Email address is required',
-		validate: {
-			validator: validator.isEmail,
-			message: '{VALUE} is not a valid email',
-			isAsync: false
+		name: String,
+		email: {
+			type: String,
+			trim: true,
+			lowercase: true,
+			unique: true,
+			index: { unique: true },
+			required: 'Email address is required',
+			validate: {
+				validator: validator.isEmail,
+				message: '{VALUE} is not a valid email',
+				isAsync: false
+			}
 		}
-	},
-	password: {
-		type: String,
-		required: 'Password is required',
-		match: [/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, '{VALUE} is not a valid password']
-	}
-});
+	}),
+	passwordValidator = (password, cb) => {
+		var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+		if (!password.match(regex)) {
+			return cb(password + 'is not a valid password');
+		}
+		return cb();
+	};
 
 UserSchema.set('timestamps', true);
-UserSchema.plugin(passportLocalMongoose);
-
-UserSchema.pre('save', function (next) {
-	var user = this;
-
-	// Only hash the password if it has been modified (or is new)
-	if (!user.isModified('password')) return next();
-
-	// Generate a salt
-	bcrypt.genSalt(10, (err, salt) => {
-		if (err) return next(err);
-
-		// Hash the password using our new salt
-		bcrypt.hash(user.password, salt, (err, hash) => {
-			if (err) return next(err);
-
-			// Override the cleartext password with the hashed one
-			user.password = hash;
-			return next();
-		});
-	});
+UserSchema.plugin(passportLocalMongoose, {
+	usernameField: 'email',
+	usernameLowerCase: true,
+	passwordValidator: passwordValidator,
+	errorMessages: {
+		IncorrectPasswordError: 'Password incorrect',
+		IncorrectUsernameError: 'There is no account registered with that email',
+		UserExistsError: 'Oops! Account with same email already exists! Try logging in or use a different email.'
+	}
 });
-
-UserSchema.methods.comparePassword = function (candidatePassword, cb) {
-	bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-		if (err) return cb(err);
-
-		return cb(null, isMatch);
-	});
-};
 
 module.exports = mongoose.model('Users', UserSchema);
