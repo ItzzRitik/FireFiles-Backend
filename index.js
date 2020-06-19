f3a73a77be649622b1a3b7bab40aeb17ea25b4dd
Ritik Srivastava <ritik.space@gmail.com>
2020-06-19 21:53:58 +0530

    Initialize passport

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const express = require('express'),
 	app = express(),
+	session = require('express-session'),
 	http = require('http'),
 	server = http.createServer(app),
 	bodyparser = require('body-parser'),
 	ip = require('ip'),
 	chalk = require('chalk'),
 
-	mongoUtils = require('./tools/mongoUtils'),
+	passport = require('passport'),
+
+	dbUtils = require('./tools/dbUtils'),
 	logger = require('./tools/logger');
 	
 require('dotenv').config();
 const env = process.env;
 
 app.set('view engine', 'ejs');
+
 app.use('/public', express.static('public'));
 app.use('/lib', express.static('node_modules'));
 app.use(bodyparser.json({ limit: '50mb' }));
 app.use(bodyparser.urlencoded({ limit: '50mb', extended: true, parameterLimit:50000 }));
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
+
+app.use(session({
+	secret: env.SESSION_KEY,
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+dbUtils.initPassport(passport);
 
 app.post('/login', (req, res) => {
 	let credential = {
@@ -29,7 +38,7 @@ app.post('/login', (req, res) => {
 		password: req.body.password
 	};
 
-	mongoUtils.login(credential, (err, status) => {
+	dbUtils.login(credential, (err, status) => {
 		if (err) {
 			logger.error('Error occurred while logging in', credential.email, err);
 			res.status(500).send('Apologies! Unexpected error occurred while logging in!');
@@ -56,7 +65,7 @@ app.post('/signup', (req, res) => {
 		password: req.body.password
 	};
 
-	mongoUtils.signup(userData, (err, status) => {
+	dbUtils.signup(userData, (err, status) => {
 		if (err) {
 			logger.error('Error occurred while creating account', err);
 			res.status(500).send('Apologies! Unexpected error occured while creating account!');
@@ -87,6 +96,6 @@ server.listen(env.PORT || 8080, () => {
 	logger.log(false, 'Server is running at', 
 		chalk.blue('http://' + (env.IP || ip.address() || 'localhost') + ':' + (env.PORT || '8080')));
 	
-	mongoUtils.connect(() => {
+	dbUtils.connectMongoDB(() => {
 	});
 });
