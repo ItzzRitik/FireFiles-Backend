fd8b939da1e69b8a247f97c3eadce9594b17b271
Ritik Srivastava <ritik.space@gmail.com>
2020-06-21 03:34:05 +0530

    New colors on login page

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -3,6 +3,7 @@ window.onload = function () {
 	$.post('/getUser', function(user, status) {
 		if (status === 'success' && user) {
 			window.user = user;
+			$('a').text(window.user.name);
 		}
 
 	}, 'json');
