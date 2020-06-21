3333d812912e291b48ac949d2decf89fbe424c23
Ritik Srivastava <ritik.space@gmail.com>
2020-06-21 20:36:12 +0530

    uploading file to S3

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -8,6 +8,42 @@ window.onload = function () {
 	}, 'json');
 };
 
+function uploadFile (file, signedRequest) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('PUT', signedRequest);
+	xhr.setRequestHeader('Content-type', file.type);
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				alert('Picture uploaded to S3', xhr.responseText);
+			}
+			else {
+				alert('Could not upload file.');
+			}
+		}
+	};
+	xhr.send(file);
+}
+
+
 $('.upload input').change(function () {
-	console.log($(this).val());
+	const file = this.files[0],
+		xhr = new XMLHttpRequest();
+	xhr.open('POST', '/getSignedS3');
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				console.log(xhr.responseText);
+				uploadFile(file, xhr.responseText);
+			}
+			else {
+				alert('Could not get signed URL.');
+			}
+		}
+	};
+	xhr.send(JSON.stringify({
+		fileName: file.name,
+		fileType: file.type
+	}));
 });
