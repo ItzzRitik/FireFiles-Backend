6e94a8cc2ee7910e6072b237d533d9a508b0abd5
Ritik Srivastava <ritik.space@gmail.com>
2020-06-21 20:36:28 +0530

    rename http to xhr

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -5,28 +5,28 @@ $('.overlayContainer').click(function () {
 });
 
 $('#signIn').click(function () {
-	const http = new XMLHttpRequest();
-	http.open('POST', '/login');
-	http.setRequestHeader('Content-type', 'application/json');
-	http.onreadystatechange = function() {
-		if (http.readyState == XMLHttpRequest.DONE) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', '/login');
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
 			new Noty({
-				text: (http.status == 200) ? 'Yayy! Successfully logged in!' : http.responseText,
-				type: (http.status == 200) ? 'success' : 'error',
+				text: (xhr.status == 200) ? 'Yayy! Successfully logged in!' : xhr.responseText,
+				type: (xhr.status == 200) ? 'success' : 'error',
 				theme: notyTheme,
 				layout: (screen.width <= 480) ? 'bottomCenter' : 'topRight',
 				timeout: 5000
 			}).show();
 
-			if (http.status == 200) {
+			if (xhr.status == 200) {
 				setTimeout(function() {
 					window.location = '/dashboard';
 				}, 2000);
 			}
-			else if (http.status == 400) {
+			else if (xhr.status == 403) {
 				// Incorrect password
 			}
-			else if (http.status == 404) {
+			else if (xhr.status == 404) {
 				// User not found
 			}
 			else {
@@ -34,27 +34,27 @@ $('#signIn').click(function () {
 			}
 		}
 	};
-	http.send(JSON.stringify({
+	xhr.send(JSON.stringify({
 		email: $('#emailLogin').val(),
 		password: $('#passwordLogin').val()
 	}));
 });
 
 $('#signUp').click(function () {
-	const http = new XMLHttpRequest();
-	http.open('POST', '/signup');
-	http.setRequestHeader('Content-type', 'application/json');
-	http.onreadystatechange = function() {
-		if (http.readyState == XMLHttpRequest.DONE) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', '/signup');
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
 			new Noty({
-				text: http.responseText,
-				type: (http.status == 201) ? 'success' : 'error',
+				text: xhr.responseText,
+				type: (xhr.status == 201) ? 'success' : 'error',
 				theme: notyTheme,
 				layout: (screen.width <= 480) ? 'bottomCenter' : 'topRight',
 				timeout: 5000
 			}).show();
 
-			if (http.status == 201 || http.status == 400) {
+			if (xhr.status == 201 || xhr.status == 400) {
 				$('#emailLogin').val($('#emailSignUp').val());
 				$('#emailSignUp').val('');
 
@@ -64,7 +64,7 @@ $('#signUp').click(function () {
 			}
 		}
 	};
-	http.send(JSON.stringify({
+	xhr.send(JSON.stringify({
 		name: $('#nameSignUp').val(),
 		email: $('#emailSignUp').val(),
 		password: $('#passwordSignUp').val()
