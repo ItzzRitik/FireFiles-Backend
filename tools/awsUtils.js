eefce387af3d9c697fe6709cdaa5a6af27fe10cd
Ritik Srivastava <ritik.space@gmail.com>
2020-06-22 08:43:54 +0530

    AWS download file using unbuffered stream

--- tools/awsUtils.js
+++ tools/awsUtils.js
@@ -1,5 +1,8 @@
+const { HmacSHA384 } = require('crypto-js');
+
 require('dotenv').config();
 const aws = require('aws-sdk'),
+
 	awsSecrets = {
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -9,23 +12,35 @@ const aws = require('aws-sdk'),
 aws.config.update(awsSecrets);
 	
 const getSignedS3 = (file, user, cb) => {
-	const params = {
-		Bucket: process.env.AWS_S3_BUCKET,
-		Key: user.id + '/' + file.name,
-		Expires: 60,
-		ContentType: file.type,
-		ACL: 'private'
+		const params = {
+			Bucket: process.env.AWS_S3_BUCKET,
+			Expires: 60,
+			ACL: 'private',
+			Conditions: [['content-length-range', 100, 10000000]],
+			Fields: {
+				'Content-Type': file.type,
+				'key': user.id + '/' + file.name
+			}
+		};
+
+		new aws.S3().createPresignedPost(params, (err, preSignedPost) => {
+			if (err) return cb(err);
+
+			return cb(null, preSignedPost);
+		});
+	},
+
+	streamFile = (fileKey, cb) => {
+		console.log('Trying to download file', fileKey);
+		var options = {
+			Bucket: process.env.AWS_S3_BUCKET,
+			Key: fileKey,
+		};
+
+		new aws.S3().getObject(options).on('httpHeaders', function (statusCode, headers) {
+			let readStream = this.response.httpResponse.createUnbufferedStream();
+			cb(headers, readStream);
+		}).send();
 	};
 
-	new aws.S3().getSignedUrl('putObject', params, (err, signedUrl) => {
-		if (err) return cb(err);
-
-		// const signedUrl = {
-		// 	signedRequest: data,
-		// 	url: `https://${params.Bucket}.s3.amazonaws.com/${file.name}`
-		// };
-		return cb(null, signedUrl);
-	});
-};
-
-module.exports = { getSignedS3 };
+module.exports = { getSignedS3, streamFile };
